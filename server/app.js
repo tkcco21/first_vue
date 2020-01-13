e0c9f57eebe90519b92cd5b604e2e6c2f860e95d
tkcco <asd33tyu@gmail.com>
2020-01-13 09:54:45 +0900

    corsパッケージ入れた

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import path from 'path';
-
+import cors from 'cors';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
@@ -27,13 +26,7 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// CORSを許可する
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS');
-  next();
-});
+app.use(cors());
 
 app.use('/api', apiRouter);
 
